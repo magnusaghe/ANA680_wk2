1a5f4b596540b97b332939cec0c4650edaec2372
Magnus Aghe <magnusaghe2016@gmail.com>
2023-11-05 04:51:15 -0600

    Initialize Git repository

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
-import numpy as np
-import pickle
-#import joblib
+
 app = Flask(__name__)
 filename = 'file_breastcancer.pkl'
 model = pickle.load(open(filename, 'rb'))    # load the model
@@ -14,7 +12,7 @@ def predict():
     Uniformity_of_cell_shape = request.form['uniformity_cell_shape']
     Uniformity_of_cell_size = request.form['uniformity_cell_size']
     Bland_Chromatin = request.form['bland_chromatin']
-    pred = model.predict(np.array([[Bare_Nuclei, Uniformity_of_cell_shape, Uniformity_of_cell_size, Bland_Chromatin]]))
+    pred = model.predict(np.array([[Bare_Nuclei, Uniformity_of_cell_shape, Uniformity_of_cell_size, Bland_Chromatin ]]))
     #print(pred)
     return render_template('index.html', predict=str(pred))
 if __name__ == '__main__':
