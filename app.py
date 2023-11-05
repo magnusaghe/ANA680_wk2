501f013973cea1f1ff46c8710239921bcc869454
Magnus Aghe <magnusaghe2016@gmail.com>
2023-11-05 12:39:29 -0600

    Initialize Git repository

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def predict():
     #print(pred)
     return render_template('index.html', predict=str(pred))
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.9",port=8080, debug=True)
